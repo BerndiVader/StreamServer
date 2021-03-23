9fc92fd963c9e1b651f50955438e4948a2252eef
BerndiVader <kasermandel@gmail.com>
2021-03-23 20:19:12 +0100

    changed access of static classes

--- src/main/java/com/gmail/berndivader/streamserver/StreamServer.java
+++ src/main/java/com/gmail/berndivader/streamserver/StreamServer.java
@@ -3,23 +3,30 @@ package com.gmail.berndivader.streamserver;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import com.gmail.berndivader.streamserver.config.Config;
 import com.gmail.berndivader.streamserver.ffmpeg.BroadcastRunner;
+import com.gmail.berndivader.streamserver.mysql.DatabaseConnection;
 
 public class StreamServer {
 	
-	static BroadcastRunner BROADCASTRUNNER;
-	static ConsoleRunner CONSOLERUNNER;
-	static Config CONFIG;
-	public static void main(String[] args) throws GeneralSecurityException, IOException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+	public static BroadcastRunner BROADCASTRUNNER;
+	public static ConsoleRunner CONSOLERUNNER;
+	public static Config CONFIG;
+	public static DatabaseConnection DATABASECONNECTION;
+	
+	public static void main(String[] args) throws GeneralSecurityException, IOException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException, ClassNotFoundException, SQLException {
 		
 		CONFIG=new Config();
+		DATABASECONNECTION=new DatabaseConnection();
 		BROADCASTRUNNER=new BroadcastRunner();
 		CONSOLERUNNER=new ConsoleRunner();
 		BROADCASTRUNNER.stop();
+		Helper.close();
+		
 	}
 	
 }
