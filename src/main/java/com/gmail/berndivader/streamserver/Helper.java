619f0edec0a4ea47192449aaaa7c93ab146c76a6
BerndiVader <kasermandel@gmail.com>
2021-04-08 10:39:42 +0200

    added scheduledexecutorservice. added console output on close.

--- src/main/java/com/gmail/berndivader/streamserver/Helper.java
+++ src/main/java/com/gmail/berndivader/streamserver/Helper.java
@@ -3,18 +3,26 @@ package com.gmail.berndivader.streamserver;
 import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Helper {
 	
 	public static ExecutorService executor;
+	public static ScheduledExecutorService scheduledExecutor;
 	public static File[] files;
 	
 	static {
 		executor=Executors.newFixedThreadPool(10);
+		scheduledExecutor=Executors.newScheduledThreadPool(5);
 	}
 	
 	public static void close() {
+		ConsoleRunner.print("Shutdown task executor...");
 		executor.shutdown();
+		ConsoleRunner.println("DONE!");
+		ConsoleRunner.print("Shutdown scheduled task executor...");
+		scheduledExecutor.shutdown();
+		ConsoleRunner.println("DONE!");
 	}
 
 }
