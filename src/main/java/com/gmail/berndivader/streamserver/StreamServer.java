26cc11cff0a16dbc9f30b0da7df5f27d00383b86
BerndiVader <kasermandel@gmail.com>
2021-04-08 10:40:29 +0200

    added discordbot. added forceexit option on shutdown.

--- src/main/java/com/gmail/berndivader/streamserver/StreamServer.java
+++ src/main/java/com/gmail/berndivader/streamserver/StreamServer.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import com.gmail.berndivader.streamserver.config.Config;
+import com.gmail.berndivader.streamserver.discord.DiscordBot;
 import com.gmail.berndivader.streamserver.ffmpeg.BroadcastRunner;
 import com.gmail.berndivader.streamserver.mysql.DatabaseConnection;
 
@@ -17,16 +18,26 @@ public class StreamServer {
 	public static ConsoleRunner CONSOLERUNNER;
 	public static Config CONFIG;
 	public static DatabaseConnection DATABASECONNECTION;
+	public static DiscordBot DISCORDBOT;
 	
 	public static void main(String[] args) throws GeneralSecurityException, IOException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException, ClassNotFoundException, SQLException {
 		
 		CONFIG=new Config();
 		DATABASECONNECTION=new DatabaseConnection();
 		BROADCASTRUNNER=new BroadcastRunner();
+		DISCORDBOT=new DiscordBot();
 		CONSOLERUNNER=new ConsoleRunner();
+		
 		BROADCASTRUNNER.stop();
+		DISCORDBOT.close();
 		Helper.close();
 		
+		if(ConsoleRunner.forceExit) {
+			ConsoleRunner.println("[FORCE EXIT]");
+			System.exit(0);
+		} else {
+			ConsoleRunner.println("[FINISH ALL RUNNING TASKS, THEN EXIT]");
+		}
 	}
 	
 }
