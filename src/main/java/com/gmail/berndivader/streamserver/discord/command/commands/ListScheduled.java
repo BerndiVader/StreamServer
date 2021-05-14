09ed8d87850a862e1a4e18a7dff4acf059e4ca27
BerndiVader <kasermandel@gmail.com>
2021-05-14 20:10:38 +0200

    added listscheduled console command

---
+++ src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/ListScheduled.java
@@ -0,0 +1,57 @@
+package com.gmail.berndivader.streamserver.discord.command.commands;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
+
+import com.gmail.berndivader.streamserver.annotation.DiscordCommand;
+import com.gmail.berndivader.streamserver.console.ConsoleRunner;
+import com.gmail.berndivader.streamserver.discord.command.Command;
+import com.gmail.berndivader.streamserver.mysql.GetAllScheduled;
+
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import reactor.core.publisher.Mono;
+
+@DiscordCommand(name="allscheduled")
+public class ListScheduled extends Command<Message>{
+
+	@Override
+	public Mono<Message> execute(String string, MessageChannel channel) {
+		return channel.createEmbed(new Consumer<EmbedCreateSpec>() {
+
+			@Override
+			public void accept(EmbedCreateSpec embed) {
+				GetAllScheduled scheduled=new GetAllScheduled();
+				ArrayList<String>files=null;
+				try {
+					files=scheduled.future.get(20,TimeUnit.SECONDS);
+				} catch (InterruptedException | ExecutionException | TimeoutException e) {
+					ConsoleRunner.println(e.getMessage());
+				}
+				embed.setTitle("SCHEDULED FILES");
+				if(files!=null&&!files.isEmpty()) {
+					embed.setColor(Color.CINNABAR);
+					StringBuilder builder=new StringBuilder();
+					builder.append("```");
+					int size=files.size();
+					for(int i1=0;i1<size;i1++) {
+						builder.append(files.get(i1));
+						builder.append("\n");
+					}
+					builder.append("```");
+					embed.setDescription(builder.toString());
+				} else {
+					embed.setColor(Color.RED);
+					embed.setDescription("```No scheduled files.```");
+				}
+			}
+			
+		});
+	}
+
+}
---
+++ src/main/java/com/gmail/berndivader/streamserver/mysql/GetAllScheduled.java
@@ -0,0 +1,45 @@
+package com.gmail.berndivader.streamserver.mysql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+import com.gmail.berndivader.streamserver.console.ConsoleRunner;
+import com.gmail.berndivader.streamserver.Helper;
+
+public class GetAllScheduled implements Callable<ArrayList<String>> {
+	
+	public Future<ArrayList<String>>future;
+	
+	public GetAllScheduled() {
+		
+		future=Helper.executor.submit(this);
+		
+	}
+
+	@Override
+	public ArrayList<String> call() throws Exception {
+		ArrayList<String>files=new ArrayList<>();
+		try(Connection connection=DatabaseConnection.getNewConnection()) {
+			try(PreparedStatement scheduled=connection.prepareStatement("SELECT * FROM `scheduled`",ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY)) {
+				ResultSet result=scheduled.executeQuery();
+				result.beforeFirst();
+				while(result.next()) {
+					files.add(result.getString("filename"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			ConsoleRunner.println("\n[MYSQL GET SCHEDULED LIST FAILED]");
+			return null;
+		}
+		
+		ConsoleRunner.println("\n[MYSQL GET SCHEDULED LIST SUCCSEED]");
+		return files;
+	}
+
+}
