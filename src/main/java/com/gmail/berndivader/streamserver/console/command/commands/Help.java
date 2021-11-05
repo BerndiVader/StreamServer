b120c1687d5ae13c6bb52bdb241a4a5c3ea55672
BerndiVader <kasermandel@gmail.com>
2021-11-05 20:50:59 +0100

    added command prefix

--- src/main/java/com/gmail/berndivader/streamserver/console/command/commands/Help.java
+++ src/main/java/com/gmail/berndivader/streamserver/console/command/commands/Help.java
@@ -1,9 +1,11 @@
 package com.gmail.berndivader.streamserver.console.command.commands;
 
+import com.gmail.berndivader.streamserver.annotation.ConsoleCommand;
 import com.gmail.berndivader.streamserver.config.Config;
 import com.gmail.berndivader.streamserver.console.ConsoleRunner;
 import com.gmail.berndivader.streamserver.console.command.Command;
 
+@ConsoleCommand(name="h")
 public class Help extends Command {
 
 	@Override
