590d7f7f21ed0a34baefd31a19ebb610760e9d0d
BerndiVader <kasermandel@gmail.com>
2023-02-23 18:35:26 +0100

    fix conflicts

--- src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/Schedule.java
+++ src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/Schedule.java
@@ -8,31 +8,22 @@ import com.gmail.berndivader.streamserver.annotation.DiscordCommand;
 import com.gmail.berndivader.streamserver.discord.command.Command;
 import com.gmail.berndivader.streamserver.mysql.AddScheduled;
 
-import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
 import reactor.core.publisher.Mono;
 
 @DiscordCommand(name="schedule")
-public class Schedule extends Command<Message> {
+public class Schedule extends Command<Void> {
 
 	@Override
-<<<<<<< HEAD
 	public Mono<Void> execute(String p, MessageChannel channel) {
 		
-=======
-	public Mono<Message> execute(String p, MessageChannel channel) {
-		
-		Mono<Message>mono=Mono.empty();
-		
->>>>>>> a25f33e71db747c369d305e9a33fc4d3154e2a7f
 		int index=Utils.getFilePosition(p);
 		if(index==-1) {
 			index=Utils.getCustomFilePosition(p);
 			if(index>-1) {
 				new AddScheduled(Helper.customs[index].getName());
-<<<<<<< HEAD
 			}
 		} else {
 			new AddScheduled(p);
@@ -56,41 +47,9 @@ public class Schedule extends Command<Message> {
 				embed.setColor(Color.RED);
 				embed.setTitle("No file found for");
 				embed.setDescription(p);
-=======
->>>>>>> a25f33e71db747c369d305e9a33fc4d3154e2a7f
 			}
-		} else {
-			new AddScheduled(p);
-		}
-		if(index!=-1) {
-			mono=channel.createEmbed(new Consumer<EmbedCreateSpec>() {
 
-<<<<<<< HEAD
 		}).then();
-=======
-				@Override
-				public void accept(EmbedCreateSpec embed) {
-					embed.setColor(Color.BROWN);
-					embed.setTitle("Scheduled");
-					embed.setDescription("```"+p+"```");
-				}
-
-			});
-		} else {
-			mono=channel.createEmbed(new Consumer<EmbedCreateSpec>() {
-
-				@Override
-				public void accept(EmbedCreateSpec embed) {
-					embed.setColor(Color.RED);
-					embed.setTitle("No file found for");
-					embed.setDescription("```"+p+"```");
-				}
-
-			});
-		}
-		
-		return mono;
->>>>>>> a25f33e71db747c369d305e9a33fc4d3154e2a7f
 				
 	}
 }
