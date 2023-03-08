83c7368439040778c4044017ddeb2d534db78b6c
BerndiVader <kasermandel@gmail.com>
2023-03-08 08:45:34 +0100

    added clear mysql tables runnable

---
+++ src/main/java/com/gmail/berndivader/streamserver/mysql/WipeDatabase.java
@@ -0,0 +1,39 @@
+package com.gmail.berndivader.streamserver.mysql;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.gmail.berndivader.streamserver.Helper;
+
+public class WipeDatabase implements Runnable {
+	
+	public WipeDatabase() throws InterruptedException, ExecutionException, TimeoutException {
+		
+		Future<?>future=Helper.executor.submit(this);
+		future.get(10,TimeUnit.SECONDS);
+		
+	}
+
+	@Override
+	public void run() {
+		try(Connection connection=DatabaseConnection.getNewConnection()) {
+			try(Statement wipe=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY)) {
+				wipe.addBatch("START TRANSACTION;");
+				wipe.addBatch("TRUNCATE TABLE current;");
+				wipe.addBatch("TRUNCATE TABLE playlist;");
+				wipe.addBatch("TRUNCATE TABLE scheduled;");
+				wipe.addBatch("COMMIT;");
+				wipe.executeBatch();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
