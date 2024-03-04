90945fe8e884fa1b6b87fb87791c03313f5c3faa
BerndiVader <kasermandel@gmail.com>
2024-03-04 19:06:11 +0100

    using packets instead of json

--- src/main/java/com/gmail/berndivader/streamserver/console/command/commands/LivestreamsById.java
+++ src/main/java/com/gmail/berndivader/streamserver/console/command/commands/LivestreamsById.java
@@ -6,39 +6,28 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import com.gmail.berndivader.streamserver.annotation.ConsoleCommand;
-import com.gmail.berndivader.streamserver.console.ConsoleRunner;
 import com.gmail.berndivader.streamserver.console.command.Command;
 import com.gmail.berndivader.streamserver.youtube.Youtube;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.gmail.berndivader.streamserver.youtube.packets.EmptyPacket;
+import com.gmail.berndivader.streamserver.youtube.packets.Packet;
 
-@ConsoleCommand(name="streamby")
+@ConsoleCommand(name="streamby",usage="get livestream by id")
 public class LivestreamsById extends Command{
 
 	@Override
 	public boolean execute(String[] args) {
-		JsonObject json=null;
+		Packet packet=null;
 		for(int i=0;i<args.length;i++) {
 			if(args[i].length()==0) continue;
-			Future<JsonObject>future=Youtube.livestreamsByChannelId(args[i]);
+			Future<Packet>future=Youtube.livestreamsByChannelId(args[i]);
 			try {
-				json=future.get(15l,TimeUnit.SECONDS);
+				packet=future.get(15l,TimeUnit.SECONDS);
 			} catch (InterruptedException | ExecutionException | TimeoutException e) {
 				e.printStackTrace();
 				return false;
 			}
-			if(json.get("error")==null) {
-				if(json.get("items").isJsonArray()) {
-					JsonArray array=json.get("items").getAsJsonArray();
-					int size=array.size();
-					for(int j=0;j<size;j++) {
-						JsonObject item=array.get(j).getAsJsonObject();
-						ConsoleRunner.println(item.toString());
-					}
-				}
-			}else {
-				
-			}
+			if(packet==null||packet instanceof EmptyPacket) return false;
+
 		}
 		return true;
 	}
--- src/main/java/com/gmail/berndivader/streamserver/youtube/Youtube.java
+++ src/main/java/com/gmail/berndivader/streamserver/youtube/Youtube.java
@@ -4,29 +4,36 @@ import java.util.concurrent.Future;
 
 import com.gmail.berndivader.streamserver.Helper;
 import com.gmail.berndivader.streamserver.config.Config;
+import com.gmail.berndivader.streamserver.youtube.packets.ErrorPacket;
+import com.gmail.berndivader.streamserver.youtube.packets.LiveStreamPacket;
+import com.gmail.berndivader.streamserver.youtube.packets.Packet;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class Youtube {
 	
 	private static final String URL="https://youtube.googleapis.com/youtube/v3/";
 	
-	public static Future<JsonObject> livestreamsByChannelId(String id) {
+	public static Future<Packet> livestreamsByChannelId(String id) {
 		
 		String query=URL.concat("search?part=snippet&eventType=live&maxResults=1&type=video&prettyPrint=true&channelId=").concat(id).concat("&key=").concat(Config.YOUTUBE_KEY);
-		System.err.println(query);
+		System.out.println(query);
 		
-		return Helper.executor.submit(new GetHttpCallable<JsonObject>(query) {
+		return Helper.executor.submit(new Response<Packet>(query) {
 			
 			@Override
-			protected JsonObject handle(JsonObject json) {
-				System.err.println(json.toString());
-				return json;
+			protected Packet handle(JsonObject json) {
+				if(json.has("error")) handleErr(json.get("error").getAsJsonObject());
+				LiveStreamPacket packet=new Gson().fromJson(json,LiveStreamPacket.class);
+				packet.source=json;
+				return packet;
 			}
 
 			@Override
-			protected JsonObject handleErr(JsonObject json) {
-				System.err.println(json.toString());
-				return json;
+			protected Packet handleErr(JsonObject json) {
+				ErrorPacket error=new Gson().fromJson(json,ErrorPacket.class);
+				error.printSimple();
+				return error;
 			}
 			
 		});
@@ -35,11 +42,3 @@ public class Youtube {
 
 }
 
-/*
- * liveBroadcasts?broadcastStatus=active&id=YOUR_BROADCAST_ID&key=[YOUR_API_KEY]
- *  
- * search?part=snippet&eventType=live&maxResults=1&q=news&type=video&prettyPrint=true&key=[YOUR_API_KEY]
- * search?part=snippet&channelId=ID&eventType=live&maxResults=1&type=video&prettyPrint=true&key=[YOUR_API_KEY]
- * 
- * 
-*/
