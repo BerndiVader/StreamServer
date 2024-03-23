5c60525ffbe95f7840565c3ba510d60bcf1cf0b4
BerndiVader <kasermandel@gmail.com>
2024-03-23 20:03:04 +0100

    load the command classes for performance reason

--- src/main/java/com/gmail/berndivader/streamserver/discord/command/Commands.java
+++ src/main/java/com/gmail/berndivader/streamserver/discord/command/Commands.java
@@ -1,6 +1,7 @@
 package com.gmail.berndivader.streamserver.discord.command;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URLDecoder;
@@ -14,19 +15,19 @@ import com.gmail.berndivader.streamserver.annotation.DiscordCommand;
 import com.gmail.berndivader.streamserver.console.ConsoleRunner;
 
 public class Commands {
-	
-	final static String PACKAGE_NAME="com/gmail/berndivader/streamserver/discord/command/commands";
-	static String fileName;
-	HashMap<String,String>commands;
-	
+
+	private final static String PACKAGE_NAME="com/gmail/berndivader/streamserver/discord/command/commands";
+	private static String fileName;
+	private HashMap<String,Class<Command<?>>>commands;
+
 	static {
 		try {
-			fileName = URLDecoder.decode(
+			fileName=URLDecoder.decode(
 					StreamServer.class.getProtectionDomain().getCodeSource().getLocation().getPath(),
 					StandardCharsets.UTF_8.toString());
 		} catch (UnsupportedEncodingException e) {
 			try {
-				fileName = URLDecoder.decode(
+				fileName=URLDecoder.decode(
 						StreamServer.class.getProtectionDomain().getCodeSource().getLocation().getPath(),
 						StandardCharsets.ISO_8859_1.toString());
 			} catch (UnsupportedEncodingException e1) {
@@ -36,49 +37,44 @@ public class Commands {
 	}
 	
 	public Commands() {
-		
 		commands=new HashMap<>();
-		loadClasses();
-		
+		loadCommandClasses();
 	}
 	
-	void loadClasses() {
-		
-		try {
-			try(JarInputStream jarStream=new JarInputStream(new FileInputStream(fileName))) {
-				JarEntry entry;
-				while(jarStream.available()==1) {
-					entry=jarStream.getNextJarEntry();
-					if(entry!=null) {
-						String clazzName=entry.getName();
-						if(clazzName.endsWith(".class")&&clazzName.startsWith(PACKAGE_NAME)) {
-							clazzName=clazzName.substring(0,clazzName.length()-6).replace("/",".");
-							Class<?>clazz=Class.forName(clazzName);
-							DiscordCommand anno=clazz.getAnnotation(DiscordCommand.class);
-							if(anno!=null) {
-								commands.put(anno.name(),clazzName);
-							}
+	@SuppressWarnings("unchecked")
+	private void loadCommandClasses() {
+		try(JarInputStream jarStream=new JarInputStream(new FileInputStream(fileName))) {
+			JarEntry entry;
+			while(jarStream.available()==1) {
+				entry=jarStream.getNextJarEntry();
+				if(entry!=null) {
+					String clazzName=entry.getName();
+					if(clazzName.endsWith(".class")&&clazzName.startsWith(PACKAGE_NAME)) {
+						clazzName=clazzName.substring(0,clazzName.length()-6).replace("/",".");
+						Class<?>clazz=Class.forName(clazzName);
+						DiscordCommand anno=clazz.getAnnotation(DiscordCommand.class);
+						if(anno!=null) {
+							commands.put(anno.name(),(Class<Command<?>>)clazz);
 						}
 					}
 				}
 			}
-		} catch(Exception e) {
-			System.err.println(e);
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
 		}
-		
 	}
 	
 	public Command<?> getCommand(String name) {
 		if(name.isEmpty()) name="help";
-		String className=commands.get(name);
-		if(className!=null) {
-			try {
-				return (Command<?>) Class.forName(className).getDeclaredConstructor().newInstance();
-			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-				ConsoleRunner.println(e.getMessage());
-			}
+		Class<Command<?>>command=commands.get(name);
+		if(command==null) return null;
+		try {
+			return command.getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			ConsoleRunner.println(e.getMessage());
+			return null;
 		}
-		return null;
 	}
 	
 }
