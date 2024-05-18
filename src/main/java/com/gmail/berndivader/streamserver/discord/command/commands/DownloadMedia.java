b3c5be1622db7bc7f143441189db968272db2066
BerndiVader <kasermandel@gmail.com>
2024-05-18 17:37:30 +0200

    simplyfied download process

--- src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/DownloadMedia.java
+++ src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/DownloadMedia.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import com.gmail.berndivader.streamserver.Helper;
+import com.gmail.berndivader.streamserver.Utils;
+import com.gmail.berndivader.streamserver.Utils.InfoPacket;
 import com.gmail.berndivader.streamserver.annotation.DiscordCommand;
 import com.gmail.berndivader.streamserver.config.Config;
 import com.gmail.berndivader.streamserver.console.ConsoleRunner;
@@ -23,13 +25,25 @@ import reactor.core.publisher.Mono;
 
 @DiscordCommand(name="dlp",usage="Download media. [--no-default] [--dir] [--yes-playlist] --url <valid_url>")
 public class DownloadMedia extends Command<Void> {
-	
+
 	private class ProcessCallback implements Runnable {
+		
+		private enum Status {
+			NONE,
+			RUNNING,
+			ABORTED,
+			TIMEOUT,
+			EXISTS,
+			ERROR,
+			WARNING,
+			FINISHED
+		}
 		
 		private String line;
 		private final MessageChannel channel;
 		private final String uuid;
-		private boolean cancel=false,timeout=false;
+		
+		private Status status=Status.NONE;
 		
 		public ProcessCallback(String line,MessageChannel channel) {
 			this.line=line;
@@ -40,7 +54,6 @@ public class DownloadMedia extends Command<Void> {
 		@Override
 		public void run() {
 			File directory=new File(Config.DL_MUSIC_PATH);
-			StringBuilder filename=new StringBuilder();
 			if(!directory.exists()) {
 				directory.mkdir();
 			}
@@ -48,9 +61,9 @@ public class DownloadMedia extends Command<Void> {
 								
 				channel.createMessage(msg->{
 					msg.addEmbed(embed->{
-						embed.setTitle("ERROR DLP!");
-						embed.setColor(Color.RED);
-						embed.setDescription("There was an issue with the download directory configured in Config.DL_MUSIC_PATH. It is either a file or the directory couldnt be created by the bot.");
+						embed.setTitle("ERROR DLP!")
+						.setColor(Color.RED)
+						.setDescription("There was an issue with the download directory configured in Config.DL_MUSIC_PATH. It is either a file or the directory couldnt be created by the bot.");
 					});
 				}).subscribe();
 				
@@ -63,6 +76,7 @@ public class DownloadMedia extends Command<Void> {
 					embed.setColor(Color.GREEN);
 				});
 			}).doOnSuccess(message->{
+				
 				ProcessBuilder builder=new ProcessBuilder();
 				builder.directory(directory);
 				if(line.startsWith("--no-default")) {
@@ -87,12 +101,16 @@ public class DownloadMedia extends Command<Void> {
 				}
 				
 				String[]temp=line.split(" --");
+				String url="";
 				for(int i=0;i<temp.length;i++) {
 					if(temp[i].isEmpty()) continue;
 					if(!temp[i].startsWith("--")) temp[i]="--".concat(temp[i]);
 					String[]parse=temp[i].split(" ",2);
 					for(int j=0;j<parse.length;j++) {
-						if(parse[j].equals("--url")) continue;
+						if(parse[j].equals("--url")) {
+							if(parse.length==2) url=parse[1];
+							continue;
+						}
 						if(parse[j].equals("--dir")) {
 							if(parse.length==2) {
 								File dir=new File(Config.DL_MUSIC_PATH.concat("/").concat(parse[j+1]));
@@ -103,9 +121,9 @@ public class DownloadMedia extends Command<Void> {
 								} else {
 									message.edit(msg->{
 										msg.addEmbed(embed->{
-											embed.setTitle("Warning!");
-											embed.setColor(Color.BROWN);
-											embed.setDescription("Warning! Download directory is a file, using default.");
+											embed.setTitle("Warning!")
+											.setColor(Color.BROWN)
+											.setDescription("Warning! Download directory is a file, using default.");
 										});
 									}).subscribe();
 								}
@@ -116,22 +134,24 @@ public class DownloadMedia extends Command<Void> {
 					}
 				}
 				
+				InfoPacket infoPacket=null;
+				if(!url.isEmpty()) infoPacket=Utils.getDLPinfoPacket(url, directory);
+
 				try {
 					message.edit(msg->{
-						msg.setComponents(ActionRow.of(Button.danger(uuid,"Cancel")));
-						msg.setContent("Starting download...");
+						msg.setComponents(ActionRow.of(Button.danger(uuid,"Cancel")))
+						.setContent("Starting download...");
 					}).subscribe();
 					
 					Disposable listener=message.getClient().on(ButtonInteractEvent.class,event->{
-						if((cancel=event.getCustomId().equals(uuid))) {
+						if(event.getCustomId().equals(uuid)) {
+							status=Status.ABORTED;
 							return event.edit(msg->{
-								msg.setContent("");
-								msg.addEmbed(embed->{
-									embed.setTitle("Aborted");
-									embed.setDescription("Media download aborted by user.");
-									embed.setColor(Color.RED);
-								});
-								msg.setComponents(new ArrayList<LayoutComponent>());
+								msg.setContent("").addEmbed(embed->{
+									embed.setTitle("Aborted")
+									.setDescription("Media download aborted by user.")
+									.setColor(Color.RED);
+								}).setComponents(new ArrayList<LayoutComponent>());
 							});
 						}
 						return Mono.empty();
@@ -140,21 +160,31 @@ public class DownloadMedia extends Command<Void> {
 					Process process=builder.start();
 
 					BufferedReader input=process.inputReader();
+					StringBuilder filename=new StringBuilder();
 					long time=System.currentTimeMillis();
-					while(process.isAlive()) {
-						if(input!=null&&input.ready()) {
+					status=Status.RUNNING;
+					
+					while(status==Status.RUNNING) {
+						if(!process.isAlive()) {
+							status=Status.FINISHED;
+							break;
+						}
+						if(input.ready()) {
 							time=System.currentTimeMillis();
 							String out=input.readLine();
 							if(out.startsWith("[download] Destination:")) {
 								filename.append(out.substring(23));
 								message.edit(msg->{
-									msg.removeEmbeds();
-									msg.addEmbed(embed->{
-										embed.setTitle("Downloading");
-										embed.setDescription(filename.toString());
-										embed.setColor(Color.BLUE);
+									msg.removeEmbeds()
+									.addEmbed(embed->{
+										embed.setTitle("Downloading")
+										.setDescription(filename.toString())
+										.setColor(Color.BLUE);
 									});
 								}).subscribe();
+							} else if(out.startsWith("[ExtractAudio] Destination:")) {
+								filename.setLength(0);
+								filename.append(out.replace("[ExtractAudio] Destination: ",""));
 							} else if(out.startsWith("[download]")) {
 								message.edit(msg->{
 									msg.setContent(out);
@@ -162,59 +192,50 @@ public class DownloadMedia extends Command<Void> {
 							} else if(out.startsWith("ERROR:")) {
 								message.edit(msg->{
 									msg.setContent("").removeEmbeds().addEmbed(embed->{
-										embed.setTitle("ERROR");
-										embed.setDescription(out);
-										embed.setColor(Color.RED);
+										embed.setTitle("ERROR")
+										.setDescription(out)
+										.setColor(Color.RED);
 									});
 									msg.setComponents(new ArrayList<LayoutComponent>());
-									filename.setLength(0);
 								}).subscribe();
 							}
-						} else if(System.currentTimeMillis()-time>Config.DL_TIMEOUT_SECONDS*1000l) {
-							message.edit(msg->{
-								msg.setContent("").removeEmbeds().addEmbed(embed->{
-									embed.setTitle("TIMEOUT");
-									embed.setDescription("Download will be terminated, because it appears, that the process is stalled since "+(long)(Config.DL_TIMEOUT_SECONDS/60)+" minutes.");
-									embed.setColor(Color.RED);
-								});
-								msg.setComponents(new ArrayList<LayoutComponent>());
-							}).subscribe();
-							timeout=cancel=true;
-						}
-						if(cancel) process.destroy();
+						} else if(System.currentTimeMillis()-time>Config.DL_TIMEOUT_SECONDS*1000l) status=Status.TIMEOUT;
 					}
 					
 					BufferedReader error=process.errorReader();
-					StringBuilder string=new StringBuilder();
-					error.lines().filter(line->!line.startsWith("WARNING")).forEach(line->string.append(line));
+					StringBuilder errorBuilder=new StringBuilder();
+					error.lines().filter(line->!line.startsWith("WARNING")).forEach(line->errorBuilder.append(line));
 					
-					if(string.toString().length()>0) {
-						message.edit(msg->{
-							msg.setContent("").removeEmbeds().addEmbed(embed->{
-								embed.setTitle("WARNING");
-								embed.setColor(Color.ORANGE);
-								embed.setDescription("Something went wront. Maybe file name already exsists?\n\n".concat(string.toString()));
-							});
-							msg.setComponents(new ArrayList<LayoutComponent>());
-						}).subscribe();
-					} else {
-						message.edit(msg->{
-							msg.removeEmbeds();
-							msg.setContent("");
-							msg.addEmbed(embed->{
-								if(filename.toString().length()>0) {
-									embed.setTitle("Media download finished.");
-									embed.setDescription(filename.toString());
-									embed.setColor(Color.GREEN);
-								} else if(!timeout){
-									embed.setTitle("WARNING");
-									embed.setDescription("Mediafile already downloaded and exists.");
-									embed.setColor(Color.ORANGE);
-								}
-							});
-							msg.setComponents(new ArrayList<LayoutComponent>());
-						}).subscribe();
-					}
+					if(errorBuilder.length()>0) status=Status.ERROR;
+					
+					message.edit(msg->{
+						msg.setComponents(new ArrayList<LayoutComponent>())
+						.setContent("")
+						.addEmbed(embed->{
+							switch(status) {
+							case TIMEOUT:
+								embed.setTitle("TIMEOUT")
+								.setDescription("Download will be terminated, because it appears, that the process is stalled since "+(long)(Config.DL_TIMEOUT_SECONDS/60)+" minutes.")
+								.setColor(Color.RED);
+								break;
+							case FINISHED:
+								embed.setTitle("Media download finished.")
+								.setDescription(filename.toString())
+								.setColor(Color.GREEN);
+								break;
+							case ERROR:
+								embed.setTitle("ERROR")
+								.setColor(Color.ORANGE)
+								.setDescription("Something went wront.\n\n".concat(errorBuilder.toString()));
+								break;
+							default:
+								embed.setTitle("WARNING")
+								.setDescription("Mediafile already downloaded and exists.")
+								.setColor(Color.ORANGE);
+								break;
+							}
+						});
+					}).subscribe();
 					
 					if(listener!=null&&!listener.isDisposed()) listener.dispose();
 					if(process.isAlive()) process.destroyForcibly();
@@ -234,5 +255,5 @@ public class DownloadMedia extends Command<Void> {
 		Helper.executor.submit(new ProcessCallback(string,channel));
 		return Mono.empty();
 	}
-
+	
 }
