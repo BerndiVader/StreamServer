63744d75b3a6ae00c19fb7ebb5a2d0bcbb921b00
BerndiVader <kasermandel@gmail.com>
2024-07-05 20:09:47 +0200

    completed streambyid command

--- src/main/java/com/gmail/berndivader/streamserver/console/command/commands/LivestreamsById.java
+++ src/main/java/com/gmail/berndivader/streamserver/console/command/commands/LivestreamsById.java
@@ -10,6 +10,8 @@ import com.gmail.berndivader.streamserver.console.command.Command;
 import com.gmail.berndivader.streamserver.term.ANSI;
 import com.gmail.berndivader.streamserver.youtube.Youtube;
 import com.gmail.berndivader.streamserver.youtube.packets.EmptyPacket;
+import com.gmail.berndivader.streamserver.youtube.packets.ErrorPacket;
+import com.gmail.berndivader.streamserver.youtube.packets.LiveStreamPacket;
 import com.gmail.berndivader.streamserver.youtube.packets.Packet;
 
 @ConsoleCommand(name="streamby",usage="Get livestream by id.")
@@ -17,18 +19,41 @@ public class LivestreamsById extends Command{
 
 	@Override
 	public boolean execute(String[] args) {
-		Packet packet=null;
-		for(int i=0;i<args.length;i++) {
-			if(args[i].length()==0) continue;
-			Future<Packet>future=Youtube.livestreamsByChannelId(args[i]);
-			try {
-				packet=future.get(15l,TimeUnit.SECONDS);
-			} catch (InterruptedException | ExecutionException | TimeoutException e) {
-				ANSI.printErr("Error while waiting for streamby future.",e);
-				return false;
+		if(args.length==0) return false;
+		String arg=args[0];
+		if(arg.length()==0) return false;
+		boolean printJson=false;
+		if(arg.contains("--json")) {
+			printJson=true;
+			arg=arg.replace("--json","");
+		}
+		arg=arg.strip();
+		Packet packet=new EmptyPacket();
+		Future<Packet>future=Youtube.livestreamsByChannelId(arg);
+		try {
+			packet=future.get(15l,TimeUnit.SECONDS);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			ANSI.printErr("Error while waiting for streamby future.",e);
+		}
+		if(packet instanceof LiveStreamPacket) {
+			LiveStreamPacket lp=(LiveStreamPacket)packet;
+			if(printJson) {
+				ANSI.println(lp.toString());
+			} else {
+				String out="[BLUE]Channel: [YELLOW]"+lp.snippet.channelTitle;
+				out+="[BLUE][BR]Title: [YELLOW]"+ lp.snippet.title;
+				out+="[BLUE][BR]Description: [YELLOW]"+lp.snippet.description;
+				out+="[BLUE][BR]Publish time: [YELLOW]"+lp.snippet.publishTime;
+				out+="[BLUE][BR]Video id: [YELLOW]"+lp.id.videoId;
+				out+="[BLUE][BR]Kind: [YELLOW]"+lp.id.kind;
+				out+="[BLUE][BR]Content: [YELLOW]"+lp.snippet.liveBroadcastContent;
+				ANSI.println(out);
 			}
-			if(packet==null||packet instanceof EmptyPacket) return false;
-
+		} else if(packet instanceof EmptyPacket) {
+			ANSI.println("[YELLOW]There is no livestream broadcasting on this channel.");
+		} else if(packet instanceof ErrorPacket) {
+			ErrorPacket er=(ErrorPacket)packet;
+			er.printSimple();
 		}
 		return true;
 	}
--- src/main/java/com/gmail/berndivader/streamserver/youtube/Youtube.java
+++ src/main/java/com/gmail/berndivader/streamserver/youtube/Youtube.java
@@ -4,34 +4,43 @@ import java.util.concurrent.Future;
 
 import com.gmail.berndivader.streamserver.Helper;
 import com.gmail.berndivader.streamserver.config.Config;
+import com.gmail.berndivader.streamserver.term.ANSI;
+import com.gmail.berndivader.streamserver.youtube.packets.EmptyPacket;
 import com.gmail.berndivader.streamserver.youtube.packets.ErrorPacket;
 import com.gmail.berndivader.streamserver.youtube.packets.LiveStreamPacket;
 import com.gmail.berndivader.streamserver.youtube.packets.Packet;
-import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-public class Youtube {
+public final class Youtube {
+	
+	private Youtube() {};
 	
 	private static final String URL="https://youtube.googleapis.com/youtube/v3/";
 	
 	public static Future<Packet> livestreamsByChannelId(String id) {
 		
 		String query=URL.concat("search?part=snippet&eventType=live&maxResults=1&type=video&prettyPrint=true&channelId=").concat(id).concat("&key=").concat(Config.YOUTUBE_KEY);
-		System.out.println(query);
 		
 		return Helper.EXECUTOR.submit(new Response<Packet>(query) {
 			
 			@Override
 			protected Packet handle(JsonObject json) {
-				LiveStreamPacket packet=new Gson().fromJson(json,LiveStreamPacket.class);
-				packet.source=json;
+				LiveStreamPacket packet=new LiveStreamPacket();
+				JsonArray array=json.getAsJsonArray("items");
+				if(array.size()>0) {
+					packet=Helper.GSON.fromJson(array.get(0).getAsJsonObject(),LiveStreamPacket.class);
+					packet.source=json;
+				} else {
+					return new EmptyPacket();
+				}
 				return packet;
 			}
 
 			@Override
 			protected Packet handleErr(JsonObject json) {
-				ErrorPacket error=new Gson().fromJson(json,ErrorPacket.class);
-				error.printSimple();
+				ANSI.println(json.toString());
+				ErrorPacket error=Helper.GSON.fromJson(json,ErrorPacket.class);
 				return error;
 			}
 			
