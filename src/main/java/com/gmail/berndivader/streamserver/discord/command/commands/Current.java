1e2f041b1d4a3f00674335919e863fbcdb5b7de5
BerndiVader <kasermandel@gmail.com>
2024-07-06 23:32:22 +0200

    dc current now uses ffprobe to gather current media information

--- src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/Current.java
+++ src/main/java/com/gmail/berndivader/streamserver/discord/command/commands/Current.java
@@ -2,10 +2,13 @@ package com.gmail.berndivader.streamserver.discord.command.commands;
 
 import java.util.function.Consumer;
 
+import com.gmail.berndivader.streamserver.Helper;
 import com.gmail.berndivader.streamserver.annotation.DiscordCommand;
 import com.gmail.berndivader.streamserver.annotation.Requireds;
 import com.gmail.berndivader.streamserver.discord.command.Command;
 import com.gmail.berndivader.streamserver.ffmpeg.BroadcastRunner;
+import com.gmail.berndivader.streamserver.ffmpeg.FFProbePacket;
+import com.gmail.berndivader.streamserver.term.ANSI;
 
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.EmbedCreateSpec;
@@ -14,20 +17,27 @@ import reactor.core.publisher.Mono;
 
 @DiscordCommand(name="current",usage="Display current playing file.",requireds={Requireds.BROADCASTRUNNER})
 public class Current extends Command<Void> {
+	private FFProbePacket packet;
+	
+	public Current() {
+		packet=new FFProbePacket();
+	}
 
 	@Override
 	public Mono<Void> execute(String string, MessageChannel channel) {
-		
+		if(BroadcastRunner.currentPlaying!=null) packet=Helper.getProbePacket(BroadcastRunner.currentPlaying);
 		return channel.createEmbed(new Consumer<EmbedCreateSpec>() {
 
 			@Override
 			public void accept(EmbedCreateSpec embed) {
 				embed.setColor(Color.CINNABAR);
-				embed.setTitle("Current playing");
 				if(BroadcastRunner.currentPlaying!=null) {
-					embed.setDescription(BroadcastRunner.currentPlaying.getName());
+					embed.setTitle(packet.isSet(packet.format.tags.title)?packet.format.tags.title:"");
+					embed.setAuthor(packet.isSet(packet.format.tags.title)?packet.format.tags.title:"","","");
+					embed.setDescription(packet.isSet(packet.format.tags.description)?packet.format.tags.description:"");
+					embed.addField("Duration",packet.isSet(packet.format.duration)?packet.format.duration:"",false);
 				} else {
-					embed.setDescription("Nothing playing");
+					embed.setDescription("No media streaming.");
 				}
 			}
 			
