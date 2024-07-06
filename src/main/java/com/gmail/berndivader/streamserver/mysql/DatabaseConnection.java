620f751aaca7331ccca7e646c98a176633a6eaf8
BerndiVader <kasermandel@gmail.com>
2024-07-06 09:42:39 +0200

    method for installing tables on database.

--- src/main/java/com/gmail/berndivader/streamserver/mysql/DatabaseConnection.java
+++ src/main/java/com/gmail/berndivader/streamserver/mysql/DatabaseConnection.java
@@ -1,5 +1,6 @@
 package com.gmail.berndivader.streamserver.mysql;
 
+import java.sql.BatchUpdateException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -69,6 +70,53 @@ public class DatabaseConnection {
 	
 	public static Connection getNewConnection() throws SQLException {
 		return DriverManager.getConnection(Config.DATABASE_CONNECTION,Config.DATABASE_USER,Config.DATABASE_PWD);
+	}
+	
+	public static boolean setupMySql() {
+		if(!INIT) return false;
+		
+		try(Connection connection=getNewConnection()) {
+			try(Statement statement=connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE)) {
+				statement.addBatch("START TRANSACTION;");
+				statement.addBatch("CREATE TABLE IF NOT EXISTS `current` (`uuid` VARCHAR(512), `info` VARCHAR(512));");
+				statement.addBatch("CREATE TABLE IF NOT EXISTS `info` (`infotext` VARCHAR(50));");
+				statement.addBatch("CREATE TABLE IF NOT EXISTS `playlist` (`title` VARCHAR(512), `info` VARCHAR(512), `filepath` VARCHAR(512));");
+				statement.addBatch("CREATE TABLE if NOT EXISTS `scheduled` (`id` INT(11) AUTO_INCREMENT, `title` VARCHAR(512), `filename` VARCHAR(512), PRIMARY KEY (`id`));");
+				statement.addBatch("CREATE TABLE IF NOT EXISTS `downloadables` (`uuid` VARCHAR(36) NOT NULL, `path` VARCHAR(256) NOT NULL, `timestamp` BIGINT NOT NULL, `downloads` INT NOT NULL, `ffprobe` VARCHAR(4095));");
+				statement.addBatch("TRUNCATE `current`; TRUNCATE `info`; TRUNCATE `playlist`; TRUNCATE `scheduled`; TRUNCATE `downloadables`;");
+				statement.addBatch("COMMIT;");
+				
+				try {
+					int[]results=statement.executeBatch();
+					String out;
+					for(int i=0;i<results.length;i++) {
+						out="[YELLOW]Batchline "+i+" execute ";
+						switch (results[i]) {
+						case Statement.SUCCESS_NO_INFO:
+							out+="succeeded with unknown changed rows.";
+							break;
+						case Statement.EXECUTE_FAILED:
+							out+="failed!";
+							break;
+						default:
+							out+="suceeded with "+results[i]+" rows changed.";
+							break;
+						}
+						ANSI.println(out+"[RESET]");
+					}
+
+				} catch (BatchUpdateException e) {
+					ANSI.printErr(e.getMessage(),e);
+				}
+				
+			}
+			
+		} catch (SQLException e) {
+			ANSI.printErr("Something went wrong while setup mysql.",e);
+			return false;
+		}
+		
+		return true;
 	}
 	
 }
