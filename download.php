e3ac80b64a0e7a2fefc008af090deb1ceacfe413
BerndiVader <kasermandel@gmail.com>
2024-07-12 21:24:22 +0200

    using php buildin finfo for to get mime type fixed some security issues

--- download.php
+++ download.php
@@ -2,20 +2,30 @@
 include_once("Classes.php");
 include_once("Config.php");
 
-if(!isset($_GET["uuid"])) 
+$uuid = htmlspecialchars(trim($_GET["uuid"] ?? ''));
+if(!preg_match('/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/',$uuid)) 
 {
-    die();
+    ?>
+        Sorry, something went wrong.
+    <?php    
+    exit;
 }
 
 $entry=DatabaseTools::getEntry(htmlspecialchars($_GET["uuid"]));
-if(isset($entry)&&$entry!=false)
-{
-    $download=new FileDownload($entry["path"]);
-    $download->sendDownload();
-}
-else
+if(!$entry)
 {
     ?>
-    <b>The requested download does not exists, or the link is expired.</b>
+        The requested download does not exists, or the link is expired.
     <?php
+    exit;    
+}
+
+try {
+    $download=new FileDownload($entry["path"]);
+    $download->sendDownload();
+} catch (\InvalidArgumentException $e) {
+    ?>
+        The requested file does not exists anymore.
+    <?php
+    exit;
 }
