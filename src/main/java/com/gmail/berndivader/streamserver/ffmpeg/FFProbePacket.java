a1b65d2d1175620de4e485c77e665517fcf21ff8
BerndiVader <kasermandel@gmail.com>
2024-07-27 20:56:19 +0200

    moved build from helper to the classes itself, for cleanup

--- src/main/java/com/gmail/berndivader/streamserver/ffmpeg/FFProbePacket.java
+++ src/main/java/com/gmail/berndivader/streamserver/ffmpeg/FFProbePacket.java
@@ -1,26 +1,32 @@
 package com.gmail.berndivader.streamserver.ffmpeg;
 
-import java.nio.file.Paths;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.stream.Stream;
 
 import com.gmail.berndivader.streamserver.Helper;
+import com.gmail.berndivader.streamserver.config.Config;
+import com.gmail.berndivader.streamserver.term.ANSI;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.annotations.SerializedName;
 
 public class FFProbePacket {
 	private static final String UNKNOWN="<UNKNOWN>";
-	private static final String EMPTY="";
 	
 	private String filename=UNKNOWN;
+	private String file=UNKNOWN;
 	public Integer nb_streams=-1;
 	public Integer nb_programs=-1;
 	public Integer nb_stream_groups=-1;
 	public String format_name=UNKNOWN;
 	public String format_long_name=UNKNOWN;
-	public String start_time=EMPTY;
-	public String duration=EMPTY;
-	public String size=EMPTY;
+	public String start_time="0";
+	public String duration="0";
+	public String size="0";
 	public String bit_rate=UNKNOWN;
 	public Integer probe_score=-1;
-	public Tags tags;
+	public Tags tags=new Tags();
 		
 	public class Tags {
 		@SerializedName(value="title", alternate={"TITLE"})
@@ -34,7 +40,7 @@ public class FFProbePacket {
 		@SerializedName(value="compatible_brands", alternate={"COMPATIBLE_BRANDS"})
 		public String compatible_brands=UNKNOWN;
 		@SerializedName(value="date", alternate={"DATE"})
-		public String date=EMPTY;
+		public String date="0";
 		@SerializedName(value="encoder", alternate={"ENCODER"})
 		public String encoder=UNKNOWN;
 		@SerializedName(value="comment", alternate={"COMMENT"})
@@ -44,11 +50,13 @@ public class FFProbePacket {
 		@SerializedName(value="synopsis", alternate={"SYNOPSIS"})
 		public String synopsis=UNKNOWN;
 		@SerializedName(value="purl", alternate={"PURL"})
-		public String purl=EMPTY;
+		public String purl="";
 	}
 	
+	private FFProbePacket() {}
+	
 	public String getFileName() {
-		return Paths.get(filename).getFileName().toString();
+		return file;
 	}
 	
 	public String getPath() {
@@ -61,7 +69,50 @@ public class FFProbePacket {
 	}
 	
 	public boolean isSet(String field) {
-		return field!=null&&!field.equals(UNKNOWN)&&!field.equals(EMPTY);
+		return field!=null&&!field.equals(UNKNOWN)&&!field.isEmpty();
 	}
-
+	
+	public static FFProbePacket build(File file) {
+		FFProbePacket packet=new FFProbePacket();
+		if(file!=null&&file.exists()) {
+			ProcessBuilder builder=new ProcessBuilder();
+			builder.command("ffprobe","-v","quiet","-print_format","json","-show_format",file.getAbsolutePath());
+			try {
+				String out=Helper.startAndWaitForProcess(builder,10l);
+				if(!out.isEmpty()) {
+					JsonObject o=JsonParser.parseString(out.toString()).getAsJsonObject();
+					if(o.has("format")) {
+						Tags tags=packet.tags;
+						packet=Helper.LGSON.fromJson(o.get("format"),FFProbePacket.class);
+						packet.tags=mergeTags(tags,packet.tags);
+					}
+				}
+			} catch (Exception e) {
+				ANSI.printErr("getProbePacket method failed.", e);
+			}
+			packet.file=file.getName();
+			if(!packet.isSet(packet.tags.title)) {
+				int lastDot=packet.file.lastIndexOf('.');
+				packet.tags.title=lastDot!=-1?packet.file.substring(0,lastDot):packet.file;
+			}
+		}
+		return packet;
+	}
+	
+	private static Tags mergeTags(Tags def,Tags parsed) {
+		if(parsed==null) return def;
+		
+		Field[]fields=Tags.class.getDeclaredFields();
+		Stream.of(fields).forEach(field->{
+			try {
+				Object value=field.get(parsed);
+				if(value!=null) field.set(def,value);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				if(Config.DEBUG) ANSI.printErr(e.getMessage(),e);
+			}
+		});
+		
+	    return def;
+	}
+	
 }
--- src/main/java/com/gmail/berndivader/streamserver/ffmpeg/InfoPacket.java
+++ src/main/java/com/gmail/berndivader/streamserver/ffmpeg/InfoPacket.java
@@ -1,6 +1,8 @@
 package com.gmail.berndivader.streamserver.ffmpeg;
 
 import com.gmail.berndivader.streamserver.Helper;
+import com.gmail.berndivader.streamserver.config.Config;
+import com.gmail.berndivader.streamserver.term.ANSI;
 
 public class InfoPacket {
 		private static final String UNKNOWN="<UNKNOWN>";
@@ -24,6 +26,8 @@ public class InfoPacket {
 		public Boolean downloadable=false;
 		public Boolean temp=false;
 		
+		private InfoPacket() {}
+		
 		@Override
 		public String toString() {
 	        return Helper.LGSON.toJson(this);
@@ -31,6 +35,36 @@ public class InfoPacket {
 		
 		public boolean isSet(String field) {
 			return field!=null&&!field.equals(UNKNOWN)&&!field.equals(EMTPY);
+		}
+		
+		public static InfoPacket build(String url) {
+			ProcessBuilder builder=new ProcessBuilder();
+			builder.command("yt-dlp"
+				,"--quiet"
+				,"--no-warnings"
+				,"--dump-json"
+				,"--no-playlist"
+			);
+			if(Config.YOUTUBE_USE_COOKIES&&Config.YOUTUBE_COOKIES.exists()) {
+				builder.command().add("--cookies");
+				builder.command().add(Config.YOUTUBE_COOKIES.getAbsolutePath());
+			}
+			builder.command().add(url);
+			
+			InfoPacket info=new InfoPacket();
+			try {
+				String out=Helper.startAndWaitForProcess(builder,20l);
+				if(!out.isEmpty()) {
+					int index=out.indexOf('{');
+					if(index!=-1) out=out.substring(index);
+					info=Helper.LGSON.fromJson(out,InfoPacket.class);
+				}
+			} catch (Exception e) {
+				ANSI.printErr("getinfoPacket method failed.",e);
+			}
+			
+			if(!info.isSet(info.webpage_url)) info.webpage_url=url;
+			return info;
 		}
 
 	}
