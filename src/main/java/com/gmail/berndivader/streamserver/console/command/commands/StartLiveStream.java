b48ccb2ab26b70926329a8f77392d60725edc149
BerndiVader <kasermandel@gmail.com>
2024-07-31 22:36:08 +0200

    create new livestream&&refresh yt token

---
+++ src/main/java/com/gmail/berndivader/streamserver/console/command/commands/StartLiveStream.java
@@ -0,0 +1,33 @@
+package com.gmail.berndivader.streamserver.console.command.commands;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.gmail.berndivader.streamserver.annotation.ConsoleCommand;
+import com.gmail.berndivader.streamserver.annotation.Requireds;
+import com.gmail.berndivader.streamserver.console.command.Command;
+import com.gmail.berndivader.streamserver.ffmpeg.BroadcastRunner;
+import com.gmail.berndivader.streamserver.term.ANSI;
+import com.gmail.berndivader.streamserver.youtube.Youtube;
+import com.gmail.berndivader.streamserver.youtube.packets.Packet;
+
+@ConsoleCommand(name="startlive",usage="(title, description, privacy) - Start the livestream on Youtube.",requireds={Requireds.BROADCASTRUNNER})
+public class StartLiveStream extends Command {
+
+	@Override
+	public boolean execute(String[] args) {
+		Future<Packet>future=Youtube.createLiveStream("MCH Varo 1-4","24/7 Stream von allen alten Varo Videos.","private");
+		
+		try {
+			future.get(20l,TimeUnit.SECONDS);
+			BroadcastRunner.restart();
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			ANSI.printErr(e.getMessage(),e);
+		}
+		
+		return true;
+	}
+
+}
--- src/main/java/com/gmail/berndivader/streamserver/youtube/Youtube.java
+++ src/main/java/com/gmail/berndivader/streamserver/youtube/Youtube.java
@@ -1,6 +1,8 @@
 package com.gmail.berndivader.streamserver.youtube;
 
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -15,6 +17,7 @@ import org.apache.http.util.EntityUtils;
 
 import com.gmail.berndivader.streamserver.Helper;
 import com.gmail.berndivader.streamserver.config.Config;
+import com.gmail.berndivader.streamserver.ffmpeg.BroadcastRunner;
 import com.gmail.berndivader.streamserver.mysql.VerifyOAuth2;
 import com.gmail.berndivader.streamserver.term.ANSI;
 import com.gmail.berndivader.streamserver.youtube.packets.EmptyPacket;
@@ -28,8 +31,8 @@ import com.google.gson.JsonParser;
 public final class Youtube {
 	public static final CloseableHttpClient HTTP_CLIENT=HttpClients.createSystem();
 	private static final String URL="https://youtube.googleapis.com/youtube/v3/";
-    private static final String OAUTH_URL="https://accounts.google.com/o/oauth2/v2/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=https://www.googleapis.com/auth/youtube&state=%s";
-
+	private static final String OAUTH_URL="https://accounts.google.com/o/oauth2/v2/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=https://www.googleapis.com/auth/youtube&state=%s&access_type=offline&prompt=consent";
+	
 	private Youtube() {}
 
 	public static boolean OAuth2Flow() {
@@ -61,20 +64,29 @@ public final class Youtube {
 			post.setEntity(parameter);
 			post.setHeader("Content-Type","application/x-www-form-urlencoded");
 			
-        	String token=HTTP_CLIENT.execute(post,response->{
+			Entry<String,String>pair=HTTP_CLIENT.execute(post,response->{
+				String accessToken="";
+				String refreshToken="";
 				JsonObject json=JsonParser.parseString(EntityUtils.toString(response.getEntity())).getAsJsonObject();
-				if(json.has("access_token")) {
-					return json.get("access_token").getAsString();
-				} else if(json.has("error")) {
+				ANSI.println(json.toString());
+				if(json.has("refresh_token")) refreshToken=json.get("refresh_token").getAsString();
+				if(json.has("access_token")) accessToken=json.get("access_token").getAsString();
+				if(json.has("error")) {
 					ANSI.println("[RED]failed!");
 					ANSI.printErr(json.get("error").getAsString()+" Reason: "+json.get("error_description").getAsString(),new Throwable("OAuth2 registration failed."));
 				}
-				return "";
+				return new AbstractMap.SimpleEntry<String,String>(accessToken,refreshToken);
 			});
+			
+			String token=pair.getKey();
+			String refreshToken=pair.getValue();
         	
         	if(token.isEmpty()) throw(new Exception("Failed to receive token."));
+        	if(refreshToken.isEmpty()) throw(new Exception("Failed to receive refresh token."));
         	ANSI.println("[GREEN]done!");
         	Config.YOUTUBE_ACCESS_TOKEN=token;
+        	Config.YOUTUBE_REFRESH_TOKEN=refreshToken;
+        	Config.YOUTUBE_TOKEN_TIMESTAMP=System.currentTimeMillis()/1000;
         	Config.saveConfig();
         	
 		} catch (Exception e) {
@@ -82,6 +94,50 @@ public final class Youtube {
 			return false;
 		}
 		return true;
+	}
+	
+	private static boolean isExpired() {
+		return System.currentTimeMillis()/1000l-Config.YOUTUBE_TOKEN_TIMESTAMP>3599l;
+	}
+	
+	public static boolean refreshToken() {
+		ANSI.print("[WHITE]Try to refresh access token...");
+		HttpPost post=new HttpPost("https://oauth2.googleapis.com/token");
+	    try {
+	    	StringEntity parameter=new StringEntity(String.format("grant_type=refresh_token&client_id=%s&client_secret=%s&refresh_token=%s",
+    			Config.YOUTUBE_CLIENT_ID,
+    			Config.YOUTUBE_CLIENT_SECRET,
+    			Config.YOUTUBE_REFRESH_TOKEN
+		    ));
+	    	
+	    	post.setEntity(parameter);
+	    	post.setHeader("Content-Type","application/x-www-form-urlencoded");
+	    	
+	    	String token=HTTP_CLIENT.execute(post,response->{
+	    		
+	    		JsonObject json=JsonParser.parseString(EntityUtils.toString(response.getEntity())).getAsJsonObject();
+	    		if(json.has("access_token")) {
+	    			return json.get("access_token").getAsString();
+	    		} else {
+	    			throw new RuntimeException("Failed to refresh access token.");
+	    		}
+	    	});
+	    	
+	    	if(token.isEmpty()) throw new RuntimeException("Failed to refresh access token.");
+	    	
+	    	Config.YOUTUBE_ACCESS_TOKEN=token;
+	    	Config.YOUTUBE_TOKEN_TIMESTAMP=System.currentTimeMillis()/1000l;
+	    	Config.saveConfig();
+	    	
+			ANSI.println("[GREEN]done![PROMPT]");
+	    	return true;
+	    	
+		} catch (Exception e) {
+			ANSI.println("[RED]failed![RESET]");
+			ANSI.printErr(e.getMessage(),e);
+			return false;
+		}
+
 	}
 		
 	public static Future<Packet> livestreamsByChannelId(String id) {
@@ -105,6 +161,64 @@ public final class Youtube {
 			
 		});
 		
+	}
+	
+	public static Future<Packet> createLiveStream(String title,String description,String privacy) {
+						
+		return Helper.EXECUTOR.submit(()->{
+			if(isExpired()&&!refreshToken()) return Packet.build(new JsonObject(),EmptyPacket.class);
+			
+			String url=URL.concat("liveStreams?part=snippet,cdn,contentDetails,status&key=").concat(Config.YOUTUBE_KEY);
+			
+			HttpPost post=new HttpPost(url);
+			post.setHeader("Authorization","Bearer ".concat(Config.YOUTUBE_ACCESS_TOKEN));
+			post.addHeader("Accept","application/json");
+			post.addHeader("Content-Type","application/json");
+			
+			ANSI.println("Bearer ".concat(Config.YOUTUBE_ACCESS_TOKEN));
+			
+			JsonObject snippet=new JsonObject();
+			snippet.addProperty("title",title);
+			snippet.addProperty("description", description);
+			snippet.addProperty("isDefaultStream",true);
+			
+			JsonObject cdn=new JsonObject();
+			cdn.addProperty("frameRate","60fps");
+			cdn.addProperty("ingestionType","rtmp");
+			cdn.addProperty("resolution","1080p");
+			
+			JsonObject details=new JsonObject();
+			details.addProperty("isReusable",true);
+			
+			JsonObject status=new JsonObject();
+			status.addProperty("privacyStatus",privacy);
+			
+			JsonObject live=new JsonObject();
+			live.add("snippet",snippet);
+			live.add("cdn",cdn);
+			live.add("contentDetails",details);
+			live.add("status",status);
+			
+			post.setEntity(new StringEntity(live.toString(),"UTF-8"));
+			
+			try {
+				return HTTP_CLIENT.execute(post,response->{
+					BroadcastRunner.stopAndHold();
+					JsonObject json=JsonParser.parseString(EntityUtils.toString(response.getEntity())).getAsJsonObject();
+					if(json.has("streamName")) {
+						ANSI.println("hihi");
+						Config.STREAM_KEY=json.get("streamName").getAsString();
+						Config.saveConfig();
+					}
+					BroadcastRunner.unhold();
+					ANSI.println(json.toString());
+					return Packet.build(new JsonObject(),EmptyPacket.class); 
+				});
+			} catch (IOException e) {
+				ANSI.printErr(e.getMessage(),e);
+				return Packet.build(new JsonObject(),EmptyPacket.class);
+			}
+		});		
 	}
 	
 	public static void close() {
