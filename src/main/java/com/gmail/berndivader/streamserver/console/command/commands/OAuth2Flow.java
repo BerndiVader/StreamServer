d277a6a9c5b072ef06e6fc839ed6fb43a746d99d
BerndiVader <kasermandel@gmail.com>
2024-08-09 07:53:14 +0200

    renamed oauth2 flow cli cmd

--- src/main/java/com/gmail/berndivader/streamserver/console/command/commands/OAuth2Flow.java
+++ src/main/java/com/gmail/berndivader/streamserver/console/command/commands/OAuth2Flow.java
@@ -2,14 +2,14 @@ package com.gmail.berndivader.streamserver.console.command.commands;
 
 import com.gmail.berndivader.streamserver.annotation.ConsoleCommand;
 import com.gmail.berndivader.streamserver.console.command.Command;
-import com.gmail.berndivader.streamserver.youtube.Youtube;
+import com.gmail.berndivader.streamserver.youtube.OAuth2;
 
 @ConsoleCommand(name="oauth",usage="Start OAuth2 flow to grant access to Youtube.")
-public class OAuth2 extends Command {
+public class OAuth2Flow extends Command {
 
 	@Override
 	public boolean execute(String[] args) {
-		return Youtube.OAuth2.build();
+		return OAuth2.build();
 	}
 
 }
