104572b9fd7ed68b5e7a46342f1e4d9396affd0c
BerndiVader <kasermandel@gmail.com>
2024-08-13 14:12:19 +0200

    discord bot now only allows to be joined to permitted guilds

--- src/main/java/com/gmail/berndivader/streamserver/discord/DiscordBot.java
+++ src/main/java/com/gmail/berndivader/streamserver/discord/DiscordBot.java
@@ -3,8 +3,8 @@ package com.gmail.berndivader.streamserver.discord;
 import java.time.Duration;
 
 import com.gmail.berndivader.streamserver.config.Config;
-import com.gmail.berndivader.streamserver.discord.command.Command;
 import com.gmail.berndivader.streamserver.discord.command.Commands;
+import com.gmail.berndivader.streamserver.discord.permission.Permissions;
 import com.gmail.berndivader.streamserver.term.ANSI;
 
 import discord4j.common.ReactorResources;
@@ -48,11 +48,11 @@ public final class DiscordBot {
 			.login()
 		    .doOnSubscribe(sub->{
 		        status=Status.CONNECTING;
-		        ANSI.println("[Try to connect to Discord...]");
+		        ANSI.println("[YELLOW][Try to connect to Discord...][RESET]");
 		    })
 		    .doOnSuccess(c->{
 		        status=Status.CONNECTED;
-		        ANSI.println("[Connection to Discord OPEN!]");
+		        ANSI.println("[GREEN][Connection to Discord OPEN!][RESET]");
 		    })
 		    .doOnError(err->{
 		        status=Status.FAILED;
@@ -61,47 +61,52 @@ public final class DiscordBot {
 		
 		if(status!=Status.CONNECTED) return;
 		
+		client.on(GuildCreateEvent.class).subscribe(event->{
+			if(!Config.DISCORD_PERMITTED_GUILDS.containsKey(event.getGuild().getId().asLong())) event.getClient().logout().subscribe();
+		});
+		
 		client.on(MessageCreateEvent.class)
-			.filter(e->e.getMessage().getContent().startsWith(".")
-					&&e.getMember().isPresent()
-					&&e.getMember().get().getRoleIds().contains(Snowflake.of(Config.DISCORD_ROLE_ID)))
+	    	.filter(
+	    			e->e.getMessage().getContent().startsWith(".")
+	    			&&e.getMember().isPresent()
+	    			&&e.getGuildId().isPresent()
+	    			&&e.getMember().get().getRoleIds().contains(Snowflake.of(Config.DISCORD_ROLE_ID))
+	    			&&Permissions.Guilds.permitted(e.getGuildId().get().asLong(),e.getMessage().getChannelId().asLong())
+	    		)
 		    .flatMap(e->{
 		        Message message=e.getMessage();
 		        String content=message.getContent();
 		        String[]parse=content.split(" ",2);
-		        if(!parse[0].startsWith(".")) return Mono.empty();
-		
 		        String cmd=parse[0].toLowerCase().substring(1);
-		        Command<?>command=Commands.instance.newCommandInstance(cmd);
-		        if(command==null) return Mono.empty();
-		        String args=parse.length==2?parse[1]:"";
-		        
-                return message.getChannel().flatMap(channel->{
-                	if (channel==null) return Mono.empty();
-                	return command.exec(args,channel);
-                });
-                
+
+		        return Mono.justOrEmpty(Commands.instance.build(cmd))
+		        		.flatMap(command->{
+		        			String args=parse.length==2?parse[1]:"";
+		        			return message.getChannel().flatMap(channel->command.exec(args,channel,e.getMember().get()));
+		        		});
+
 		    })
-		    .onErrorContinue((throwable,object)->{
-		    	ANSI.printErr(throwable.getMessage(),throwable);
-		    }).subscribe();
+		    .onErrorContinue((throwable,object)->ANSI.printErr(throwable.getMessage(),throwable))
+		    .subscribe();
 		
 		client.onDisconnect().doOnSuccess(t->{
-		    status=Status.DISCONNECTED;
-		    ANSI.println("[Connection to Discord CLOSED!]");
+		    ANSI.println("[YELLOW][Connection to Discord CLOSED!][RESET]");
 		}).subscribe();
 		
 	}
 	
 	public void updateStatus(String comment) {
 		if(Config.DEBUG) ANSI.println("Set status to: "+comment);
-	    client.updatePresence(ClientPresence.of(discord4j.core.object.presence.Status.ONLINE,ClientActivity.custom(comment))).doOnError(error->ANSI.printErr("Failed to update discord status",error)).subscribe();
+	    client.updatePresence(ClientPresence.of(discord4j.core.object.presence.Status.ONLINE,ClientActivity.custom(comment)))
+	    	.onErrorContinue((throwable,object)->ANSI.printErr("Failed to update discord status",throwable))
+	    	.subscribe();
 	}
 	
 	public void close() {
 		client.getGuilds().flatMap(Guild::getChannels)
-				.filter(channel->channel.getName().equals(Config.DISCORD_CHANNEL)).flatMap(GuildChannel::delete)
+				.filter(channel->channel.getName().equals(Config.DISCORD_VOICE_CHANNEL_NAME)).flatMap(GuildChannel::delete)
 				.blockLast(Duration.ofSeconds(20));
+		status=Status.DISCONNECTED;
 		client.logout().block(Duration.ofSeconds(20));
 	}
 	
